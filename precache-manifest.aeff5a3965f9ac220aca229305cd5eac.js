30572155c83f3e511a2628f1db01a61769ae5a70
saranae00 <saranae00@gmail.com>
2019-10-21 13:04:59 +0900

    Updates

---
+++ precache-manifest.aeff5a3965f9ac220aca229305cd5eac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e9d2caea4570ceecb3763a9059e6b1",
+    "url": "/rest-area-info/index.html"
+  },
+  {
+    "revision": "7eebdf63f9f25d613e77",
+    "url": "/rest-area-info/static/css/main.78bb99cb.chunk.css"
+  },
+  {
+    "revision": "76845d56eb5dccb3eac4",
+    "url": "/rest-area-info/static/js/2.6d519798.chunk.js"
+  },
+  {
+    "revision": "7eebdf63f9f25d613e77",
+    "url": "/rest-area-info/static/js/main.763df44d.chunk.js"
+  },
+  {
+    "revision": "3319b43e528c6d8ab025",
+    "url": "/rest-area-info/static/js/runtime-main.fd88b3f5.js"
+  }
+]);
